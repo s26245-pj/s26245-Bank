fe9674615869f5276e1501e2e4ad0b2a50599138
Michał Jastrzemski <s26245@pjwstk.edu.pl>
2023-01-22 15:22:11 +0100

    Stworzenie repo zaliczenie

---
+++ src/main/java/pjatk/s26245bank/S26245BankApplication.java
@@ -0,0 +1,13 @@
+package pjatk.s26245bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26245BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S26245BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pjatk/s26245bank/S26245BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.s26245bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26245BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
